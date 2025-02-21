289e5b118eb28b9b8a8589b0370cb61c6c3b272b
Nexiauk <149090274+Nexiauk@users.noreply.github.com>
2025-02-21 20:02:41 +0000

    Add code to increment score and incorrect answers tally

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,13 +17,13 @@ document.addEventListener("DOMContentLoaded", function () {
         })
     }
 
-    runGame("addition");
 })
 
 // docstring description for function or method, creates tooltips
 
 /**The main game "loop", called when the script is first loaded
  * and after the user's answer has been processed */
+
 function runGame(gameType) {
     // Creates two random numbers between 1 and 25
     let num1 = Math.floor(Math.random() * 25) + 1;
@@ -49,8 +49,10 @@ let isCorrect = userAnswer === calculatedAnswer[0];
 
 if (isCorrect) {
     alert("Hey! You got it right! :D");
+    incrementScore();
 } else {
     alert(`Awwww..... you answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}!`);
+    incrementWrongAnswer();
 }
 
 runGame(calculatedAnswer[1]);
@@ -75,13 +77,18 @@ if (operator === "+") {
 
 }
 
+/**Gets the current score from the DOM and incremenets it by 1 */
 function incrementScore() {
-
+let oldScore = parseInt(document.getElementById("score").innerText);
+document.getElementById("score").innerText = ++oldScore;
 }
 
+/**Gets the current tally of incorrect answers from the  DOM and increments it by 1 */
 function incrementWrongAnswer() {
-
+let oldScore = parseInt(document.getElementById("incorrect").innerText);
+document.getElementById("incorrect").innerText = ++oldScore;
 }
+
 
 function displayAdditionQuestion(operand1, operand2) {
     document.getElementById("operand1").textContent = operand1;
